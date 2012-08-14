87ed6bcd0ce2b6882173e0f3710507ba6e4f8391
Maxim Dikun <dikmax@gmail.com>
2012-08-14 22:06:49 +0300

    Remove temp files.

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-java -jar compiler/compiler.jar --js src/inherited.js \
-  --js_output_file inherited.min.js \
-  --warning_level VERBOSE \
-  --compilation_level ADVANCED_OPTIMIZATIONS
--- build.testing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-java -jar compiler/compiler.jar --js src/inherited.js \
-  --js_output_file inherited.testing.js \
-  --formatting PRETTY_PRINT --warning_level VERBOSE \
-  --compilation_level ADVANCED_OPTIMIZATIONS
-
